605f49dffcfdf23541ebfd260749d8ab2f65461d
nathan <thoum.nathan@gmail.com>
2020-09-21 09:45:35 +0200

    test sauvegarde cours

---
+++ S1/ALGORITHMIQUE_AVANCE/test.py
@@ -0,0 +1,19 @@
+lst = ['r','a','m','i','t','d','t','s','m','a','l','m','i','a','m','t','i','s','y','m']
+
+target = ['m','i','a','m']
+
+ptr1 = 0
+ptr2 = 0
+
+match = False
+
+while(match == False):
+    ptr2 = ptr1 + len(target)
+    if (ptr1 == target[0]) and (ptr2 == target[len(target)]):
+            while(ptr1 != ptr2):
+                if (ptr2 - i) == (target[len(target)] - i):
+                    continue
+                else:
+                    break
+            print("ça va jusque là")
+
