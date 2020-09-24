7c0e029b55ac3d2407cebbdc355a119a7478a0dd
nathan <thoum.nathan@gmail.com>
2020-09-24 10:37:51 +0200

    suppression d'un bug

--- S1/ALGORITHMIQUE_AVANCE/unimodal.py
+++ S1/ALGORITHMIQUE_AVANCE/unimodal.py
@@ -1,4 +1,4 @@
-lst = [1, 3, 4, 5, 8,20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3,  2, 1, 0]
+lst = [1, 3, 4, 5, 8, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3,  2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9]
 
 def maximum(lst):
 
@@ -16,19 +16,20 @@ def maximum(lst):
 
     if left > mid > right:
         if (mid_point - 1) < 3:
-            return lst[:(mid_point - 1)]
+            return lst[:(mid_point)]
         left_sub_lst = lst[:mid_point]
         return maximum(left_sub_lst)
 
     if left < mid < right:
         if (len(lst) - (mid_point + 1)) < 3:
-            return lst[(mid_point + 1):]
+            return lst[(mid_point):]
         right_sub_lst = lst[mid_point:]
         return maximum(right_sub_lst)
 
 
 def max(lst):
    lst = maximum(lst)
+   print(lst)
    if len(lst) == 1:
        return lst[0]
    if lst[0] < lst[1]:
