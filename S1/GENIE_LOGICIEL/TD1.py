78cffe3eeec79d2e18a78c58d4f67281273d0cc3
nathan <thoum.nathan@gmail.com>
2020-10-08 08:37:34 +0200

    maj

--- S1/GENIE_LOGICIEL/TD1.py
+++ S1/GENIE_LOGICIEL/TD1.py
@@ -1,36 +1,61 @@
-from abc import abstract
-
-class WeatherStation:
-    __stationCounter=0
-    def __init__(self):
-        WeatherStation.__stationCounter+=1
-        self.__number=WeatherStation.__stationCounter
-    
-    def displayTemperature(self, sensorName, temperature):
-        print("Station", self.__number,"-",sensorName," > ",temperature, "degres")
+import abc
 
 class TemperatureObserver:
-    @abc abstract method 
+	@abc.abstractmethod
+	def update(event):
+		pass
 
-    def update(self, event):
-        pass
+class WeatherStation(TemperatureObserver):
+	__stationCounter=0
+	def __init__(self):
+		WeatherStation.__stationCounter+=1
+		self.__number=WeatherStation.__stationCounter
+	
+	def displayTemperature(self, sensorName, temperature):
+		print("Station", self.__number,"-",sensorName," > ",temperature, "degres")
+
+		
+class TemperatureAlarm(TemperatureObserver):
+	def __init__(self, name):
+		self.__name = name
+	def update(event):
+		print("aled")	
+
+class Thermometer:
+	def __init__(self, initalTemperature : float):
+		self.temperature = initalTemperature
+	def increaseTemperature(value : float):
+		self.temperature += value 
+		self.temperature += 0,1
+	def decreaseTemperature(value : float):
+		self.temperature -= value 
+		self.temperature -= 0,1
+	def getTemperature():
+		return self.temperature 
+
+class TemperatureChangeEvent:
+	def __init__(self, temperature : float, name : str):
+		self.temperature = temperature
+		self.sensorName = name
+	def getTemperature() -> float:
+		return self.temperature
+	def getSensorName() -> str:
+		return self.name
 
 class TemperatureSensor:
-    def __init__(self, name):
-        self.__name = name
-        self.__temperature=0
-        self.__weatherStations =[]
-    
-    def getTemperature(self):
-        return __temperature
-    
-    def setTemperature(self, temperature):
-        self.__temperature = temperature
-        for ws in self.__weatherStations:
-            ws.displayTemperature(self.__name, temperature)
-    
-    def addWeatherStation(self, ws):
-        self.__weatherStations.append(ws)
+	def __init__(self, name):
+		self.__name = name
+		self.__temperature=0
+		self.__weatherStations =[]	
+	def getTemperature(self):
+		return __temperature
+	
+	def setTemperature(self, temperature):
+		self.__temperature = temperature
+		t = TemperatureChangeEvent(temperature, self.__name)
+
+	def addWeatherStation(self, ws):
+		self.__weatherStations.append(ws)
 
 ws1 = WeatherStation()
 ws2 = WeatherStation()
@@ -52,29 +77,3 @@ print("Jardin 13 degres")
 ts3.setTemperature(13)
 print("Salon 19 degres")
 ts1.setTemperature(19)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
